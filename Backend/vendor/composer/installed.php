381c868a4ae272c18d4a9efba39aa6286e5e2f37
tvpower <tvpower89@gmail.com>
2025-04-30 13:38:35 -0700

    uhm i did something

---
+++ Backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '930e6bc091fd99f2192808b0f467a5385cada82d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '930e6bc091fd99f2192808b0f467a5385cada82d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v265',
+            'version' => '265.0.0.0',
+            'reference' => '86d85038a27f17254164d68dffbbe580f5ea58ea',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
